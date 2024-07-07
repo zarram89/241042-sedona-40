328f75ed75bfe0cdd5296782b736894473713fa2
Ramil Zaripov <zarram89@gmail.com>
2024-07-07 17:29:55 +0500

    Подготовка к защите

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,11 @@
 const modalWindow = document.querySelector(".modal-container");
 const btnCloseModal = document.querySelector(".modal-close-button");
+const btnOpenModal = document.querySelector(".button-search");
 
 btnCloseModal.addEventListener("click", function() {
-  modalWindow.style.display = "none";
+  modalWindow.classList.add("modal-container-close");
+});
+
+btnOpenModal.addEventListener("click", function() {
+  modalWindow.classList.remove("modal-container-close");
 });
